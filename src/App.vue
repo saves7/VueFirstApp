a502c1a39d5f17ddefef7233b8205429275a6d6b
saves7 <a_v.efimov@mail.ru>
2023-06-04 11:24:34 +0500

    Vue project with vue component

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+    <div>
+        Работает! Отобразилось содиржимое template App.vue
+    </div>
+    <div>
+        Колличество likes: <strong>{{ likes }}</strong>
+        Колличество dislikes: <strong>{{ dislikes }}</strong>
+    </div>
+    <button v-on:click="addLikes"> likes</button>
+    <button v-on:click="addDisLikes"> dislikes</button>  
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                likes: 0,
+                dislikes: 0,
+            }
+        },
+        methods: {
+            addLikes() {
+                this.likes += 1;
+            },
+
+            addDisLikes() {
+                this.dislikes += 1;
+            },
+
+
+        }
+    }
+</script>
+
+<style>
+</style>
